0f98fd32324624d38eafdcbcb34abf5500f2b2dd
LouisRollins <louis.rollins1@outlook.com>
2024-11-18 13:57:07 -0800

    computer list

--- Demo/services.ps1
+++ Demo/services.ps1
@@ -1,2 +1,13 @@
 # Test Sync
-get-service
+$computers = Get-ADComputer -Filter "OperatingSystem -eq 'Windows Server 2019 Datacenter Evaluation'" -Properties OperatingSystem,IPv4Address,lastLogonDate 
+
+ForEach ($computer in $computers){
+    $os = $computer.OperatingSystem
+    $ip = $computer.IPv4Address
+    $lastLogon = $computer.lastLogonDate
+    $hostname = $computer.DNSHostName
+
+    write-host $hostname "is running" $os "with an IP address of" $ip "and was last logged in at" $lastLogon
+
+}
+
